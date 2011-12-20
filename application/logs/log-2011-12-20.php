72c1dcf5842ad9f0490b3f64c2e9deba390dc220
hainuo <admin@hainuo.info>
2011-12-20 09:43:42 +0800

    日志文件

--- application/logs/log-2011-12-20.php
+++ application/logs/log-2011-12-20.php
@@ -43,3 +43,87 @@ DEBUG - 2011-12-20 09:32:38 --> Helper loaded: captcha_helper
 DEBUG - 2011-12-20 09:32:38 --> File loaded: F:\sae-hainuo\4\templates/oa/sys_login.php
 DEBUG - 2011-12-20 09:32:38 --> Final output sent to browser
 DEBUG - 2011-12-20 09:32:38 --> Total execution time: 0.1636
+DEBUG - 2011-12-20 09:41:45 --> Config Class Initialized
+DEBUG - 2011-12-20 09:41:45 --> Hooks Class Initialized
+DEBUG - 2011-12-20 09:41:45 --> Utf8 Class Initialized
+DEBUG - 2011-12-20 09:41:45 --> UTF-8 Support Enabled
+DEBUG - 2011-12-20 09:41:45 --> URI Class Initialized
+DEBUG - 2011-12-20 09:41:45 --> Router Class Initialized
+DEBUG - 2011-12-20 09:41:45 --> Output Class Initialized
+DEBUG - 2011-12-20 09:41:45 --> Security Class Initialized
+DEBUG - 2011-12-20 09:41:45 --> Input Class Initialized
+DEBUG - 2011-12-20 09:41:45 --> Global POST and COOKIE data sanitized
+DEBUG - 2011-12-20 09:41:45 --> Language Class Initialized
+DEBUG - 2011-12-20 09:41:45 --> Loader Class Initialized
+DEBUG - 2011-12-20 09:41:45 --> Controller Class Initialized
+DEBUG - 2011-12-20 09:41:45 --> Helper loaded: url_helper
+DEBUG - 2011-12-20 09:41:45 --> Session Class Initialized
+DEBUG - 2011-12-20 09:41:45 --> Helper loaded: string_helper
+DEBUG - 2011-12-20 09:41:45 --> Database Driver Class Initialized
+DEBUG - 2011-12-20 09:41:45 --> Session routines successfully run
+DEBUG - 2011-12-20 09:41:45 --> Model Class Initialized
+DEBUG - 2011-12-20 09:41:45 --> Model Class Initialized
+DEBUG - 2011-12-20 09:41:45 --> XSS Filtering completed
+DEBUG - 2011-12-20 09:41:45 --> XSS Filtering completed
+DEBUG - 2011-12-20 09:41:45 --> XSS Filtering completed
+DEBUG - 2011-12-20 09:41:45 --> Config Class Initialized
+DEBUG - 2011-12-20 09:41:45 --> Hooks Class Initialized
+DEBUG - 2011-12-20 09:41:45 --> Utf8 Class Initialized
+DEBUG - 2011-12-20 09:41:45 --> UTF-8 Support Enabled
+DEBUG - 2011-12-20 09:41:45 --> URI Class Initialized
+DEBUG - 2011-12-20 09:41:45 --> Router Class Initialized
+DEBUG - 2011-12-20 09:41:45 --> Output Class Initialized
+DEBUG - 2011-12-20 09:41:45 --> Security Class Initialized
+DEBUG - 2011-12-20 09:41:45 --> Input Class Initialized
+DEBUG - 2011-12-20 09:41:45 --> Global POST and COOKIE data sanitized
+DEBUG - 2011-12-20 09:41:45 --> Language Class Initialized
+DEBUG - 2011-12-20 09:41:45 --> Loader Class Initialized
+DEBUG - 2011-12-20 09:41:45 --> Controller Class Initialized
+DEBUG - 2011-12-20 09:41:45 --> Helper loaded: url_helper
+DEBUG - 2011-12-20 09:41:45 --> Database Driver Class Initialized
+DEBUG - 2011-12-20 09:41:45 --> Session Class Initialized
+DEBUG - 2011-12-20 09:41:45 --> Helper loaded: string_helper
+DEBUG - 2011-12-20 09:41:45 --> Session routines successfully run
+DEBUG - 2011-12-20 09:41:45 --> File loaded: F:\sae-hainuo\4\templates/oa/root/top.php
+DEBUG - 2011-12-20 09:41:45 --> File loaded: F:\sae-hainuo\4\templates/oa/sys_top.php
+DEBUG - 2011-12-20 09:41:45 --> File loaded: F:\sae-hainuo\4\templates/oa/sys_link.php
+DEBUG - 2011-12-20 09:41:45 --> File loaded: F:\sae-hainuo\4\templates/oa/hyb/left.php
+DEBUG - 2011-12-20 09:41:45 --> File loaded: F:\sae-hainuo\4\templates/oa/root/left.php
+DEBUG - 2011-12-20 09:41:45 --> File loaded: F:\sae-hainuo\4\templates/oa/sys_left.php
+DEBUG - 2011-12-20 09:41:45 --> File loaded: F:\sae-hainuo\4\templates/oa/sys_default.php
+DEBUG - 2011-12-20 09:41:45 --> File loaded: F:\sae-hainuo\4\templates/oa/sys_entry.php
+DEBUG - 2011-12-20 09:41:45 --> Final output sent to browser
+DEBUG - 2011-12-20 09:41:45 --> Total execution time: 0.1051
+DEBUG - 2011-12-20 09:41:51 --> Config Class Initialized
+DEBUG - 2011-12-20 09:41:51 --> Hooks Class Initialized
+DEBUG - 2011-12-20 09:41:51 --> Utf8 Class Initialized
+DEBUG - 2011-12-20 09:41:51 --> UTF-8 Support Enabled
+DEBUG - 2011-12-20 09:41:51 --> URI Class Initialized
+DEBUG - 2011-12-20 09:41:51 --> Router Class Initialized
+DEBUG - 2011-12-20 09:41:51 --> Output Class Initialized
+DEBUG - 2011-12-20 09:41:51 --> Security Class Initialized
+DEBUG - 2011-12-20 09:41:51 --> Input Class Initialized
+DEBUG - 2011-12-20 09:41:51 --> Global POST and COOKIE data sanitized
+DEBUG - 2011-12-20 09:41:51 --> Language Class Initialized
+DEBUG - 2011-12-20 09:41:51 --> Loader Class Initialized
+DEBUG - 2011-12-20 09:41:51 --> Controller Class Initialized
+DEBUG - 2011-12-20 09:41:51 --> Helper loaded: url_helper
+DEBUG - 2011-12-20 09:41:51 --> Database Driver Class Initialized
+DEBUG - 2011-12-20 09:41:51 --> Session Class Initialized
+DEBUG - 2011-12-20 09:41:51 --> Helper loaded: string_helper
+DEBUG - 2011-12-20 09:41:51 --> Session routines successfully run
+DEBUG - 2011-12-20 09:41:51 --> Model Class Initialized
+DEBUG - 2011-12-20 09:41:51 --> Model Class Initialized
+DEBUG - 2011-12-20 09:41:51 --> Helper loaded: form_helper
+DEBUG - 2011-12-20 09:41:51 --> Form Validation Class Initialized
+DEBUG - 2011-12-20 09:41:51 --> File loaded: F:\sae-hainuo\4\templates/oa/root/top.php
+DEBUG - 2011-12-20 09:41:51 --> File loaded: F:\sae-hainuo\4\templates/oa/sys_top.php
+DEBUG - 2011-12-20 09:41:51 --> File loaded: F:\sae-hainuo\4\templates/oa/sys_link.php
+DEBUG - 2011-12-20 09:41:51 --> File loaded: F:\sae-hainuo\4\templates/oa/hyb/left.php
+DEBUG - 2011-12-20 09:41:51 --> File loaded: F:\sae-hainuo\4\templates/oa/root/left.php
+DEBUG - 2011-12-20 09:41:51 --> File loaded: F:\sae-hainuo\4\templates/oa/sys_left.php
+ERROR - 2011-12-20 09:41:51 --> Severity: Notice  --> Undefined variable: reportDate F:\sae-hainuo\4\templates\oa\hyb\creatReport.php 5
+DEBUG - 2011-12-20 09:41:51 --> File loaded: F:\sae-hainuo\4\templates/oa/hyb/creatReport.php
+DEBUG - 2011-12-20 09:41:51 --> File loaded: F:\sae-hainuo\4\templates/oa/sys_entry.php
+DEBUG - 2011-12-20 09:41:51 --> Final output sent to browser
+DEBUG - 2011-12-20 09:41:51 --> Total execution time: 0.1650
