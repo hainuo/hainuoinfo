b741d81424f9ea37656e53ac8b3fed85c18f46f8
hainuo <admin@hainuo.info>
2011-12-20 09:33:10 +0800

    Signed-off-by: hainuo <admin@hainuo.info>

---
+++ application/logs/log-2011-12-20.php
@@ -0,0 +1,45 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+DEBUG - 2011-12-20 09:32:37 --> Config Class Initialized
+DEBUG - 2011-12-20 09:32:37 --> Hooks Class Initialized
+DEBUG - 2011-12-20 09:32:37 --> Utf8 Class Initialized
+DEBUG - 2011-12-20 09:32:37 --> UTF-8 Support Enabled
+DEBUG - 2011-12-20 09:32:37 --> URI Class Initialized
+DEBUG - 2011-12-20 09:32:37 --> Router Class Initialized
+DEBUG - 2011-12-20 09:32:37 --> Output Class Initialized
+DEBUG - 2011-12-20 09:32:37 --> Security Class Initialized
+DEBUG - 2011-12-20 09:32:37 --> Input Class Initialized
+DEBUG - 2011-12-20 09:32:37 --> Global POST and COOKIE data sanitized
+DEBUG - 2011-12-20 09:32:37 --> Language Class Initialized
+DEBUG - 2011-12-20 09:32:37 --> Loader Class Initialized
+DEBUG - 2011-12-20 09:32:37 --> Controller Class Initialized
+DEBUG - 2011-12-20 09:32:37 --> Helper loaded: url_helper
+DEBUG - 2011-12-20 09:32:37 --> Database Driver Class Initialized
+DEBUG - 2011-12-20 09:32:38 --> Session Class Initialized
+DEBUG - 2011-12-20 09:32:38 --> Helper loaded: string_helper
+DEBUG - 2011-12-20 09:32:38 --> A session cookie was not found.
+DEBUG - 2011-12-20 09:32:38 --> Session routines successfully run
+DEBUG - 2011-12-20 09:32:38 --> Config Class Initialized
+DEBUG - 2011-12-20 09:32:38 --> Hooks Class Initialized
+DEBUG - 2011-12-20 09:32:38 --> Utf8 Class Initialized
+DEBUG - 2011-12-20 09:32:38 --> UTF-8 Support Enabled
+DEBUG - 2011-12-20 09:32:38 --> URI Class Initialized
+DEBUG - 2011-12-20 09:32:38 --> Router Class Initialized
+DEBUG - 2011-12-20 09:32:38 --> Output Class Initialized
+DEBUG - 2011-12-20 09:32:38 --> Security Class Initialized
+DEBUG - 2011-12-20 09:32:38 --> Input Class Initialized
+DEBUG - 2011-12-20 09:32:38 --> Global POST and COOKIE data sanitized
+DEBUG - 2011-12-20 09:32:38 --> Language Class Initialized
+DEBUG - 2011-12-20 09:32:38 --> Loader Class Initialized
+DEBUG - 2011-12-20 09:32:38 --> Controller Class Initialized
+DEBUG - 2011-12-20 09:32:38 --> Helper loaded: url_helper
+DEBUG - 2011-12-20 09:32:38 --> Session Class Initialized
+DEBUG - 2011-12-20 09:32:38 --> Helper loaded: string_helper
+DEBUG - 2011-12-20 09:32:38 --> Database Driver Class Initialized
+DEBUG - 2011-12-20 09:32:38 --> Session routines successfully run
+DEBUG - 2011-12-20 09:32:38 --> Model Class Initialized
+DEBUG - 2011-12-20 09:32:38 --> Model Class Initialized
+DEBUG - 2011-12-20 09:32:38 --> Helper loaded: captcha_helper
+DEBUG - 2011-12-20 09:32:38 --> File loaded: F:\sae-hainuo\4\templates/oa/sys_login.php
+DEBUG - 2011-12-20 09:32:38 --> Final output sent to browser
+DEBUG - 2011-12-20 09:32:38 --> Total execution time: 0.1636
